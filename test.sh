cbe7e63718ee25082c52c6626980132b94497f16
minamotorin <76122224+minamotorin@users.noreply.github.com>
2021-01-21 16:01:43 +0000

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+program=(fizzbuzz.sed FizzBuzz_stand_alone oneliner.sed twoliner.sed)
+times=${1:-100}
+chmod u+x $program
+
+ErrorCheck(){
+  diff -q <(seq $times | sed 's/.*[05]$/Buzz/; n; s/.*[05]$/Buzz/; n; s/.*[05]$/Buzz/; s/[0-9]*/Fizz/') <(./$1 <<<$times) 2> /dev/null || echo 'Error at ' $1 >&2
+}
+
+for run in $program; do
+  ErrorCheck $run
+done
